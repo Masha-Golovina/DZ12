47a033b5f6042155b2d772bd71f744c6464e7a3d
Masha <Masha@COMPUTER>
2022-12-16 23:36:45 +0300

    Добавьте файлы проекта.

---
+++ Rational.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ12
+{
+    [DeveloperInfo("Adell", Date = "26.05.2004")]
+
+    class Rational
+    {
+        public int numerator;
+        public int denumerator;
+    }
+}
